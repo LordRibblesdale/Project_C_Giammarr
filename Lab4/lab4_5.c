75af43fa0480df45f46451edd2cb0dfb897d29a4
Domenico Verde <domenico.verde@aol.com>
2018-03-20 21:04:19 +0100

    + Add lab4_6

--- Lab4/lab4_5.c
+++ Lab4/lab4_5.c
@@ -5,3 +5,23 @@
    L'output dovrà essere del tipo:
       Il numero inserito e' ... e la massima potenza di 7 che lo divide e' .....
 */
+
+#include <stdio.h>
+
+int main(void) {
+   double input, i = 1, tmp = 0;
+
+   do {
+      printf("Insert number [xxxxxxx]: ");
+      scanf("%lf", &input);
+   } while (input <= 1000000 || input >= 10000000);
+
+   while ((7^(int)(i)) < input) {
+      if ((int)(input) % (7^(int)(i)) == 0) {
+         tmp = i;
+      }
+      i++;
+   }
+
+   printf("Il numero inserito e' %f e la massima potenza di 7 che lo divide e' %f\n", input, tmp);
+}
